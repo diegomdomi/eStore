475d7e80f9fd150f9d107e71ee5c7a7f25684508
diegomdomi <diegomdomi@gmail.com>
2022-10-18 12:38:15 +0200

    Updates

---
+++ precache-manifest.a94525e7fd28249abf1957ca3fca5ac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "026e4a3477739f1887d7",
+    "url": "/diegomdomi/eStore.git/static/css/main.aaae641d.chunk.css"
+  },
+  {
+    "revision": "026e4a3477739f1887d7",
+    "url": "/diegomdomi/eStore.git/static/js/main.87327025.chunk.js"
+  },
+  {
+    "revision": "7bfd88d8ca8253fb01d8",
+    "url": "/diegomdomi/eStore.git/static/js/runtime~main.920ca6ed.js"
+  },
+  {
+    "revision": "0a591f1af47cf478e6ab",
+    "url": "/diegomdomi/eStore.git/static/css/2.4aab178a.chunk.css"
+  },
+  {
+    "revision": "0a591f1af47cf478e6ab",
+    "url": "/diegomdomi/eStore.git/static/js/2.b60648e8.chunk.js"
+  },
+  {
+    "revision": "c832614f3909c6f83d95",
+    "url": "/diegomdomi/eStore.git/static/js/3.41c7b25b.chunk.js"
+  },
+  {
+    "revision": "113858eb18936d2c98b58176f475386f",
+    "url": "/diegomdomi/eStore.git/index.html"
+  }
+];
